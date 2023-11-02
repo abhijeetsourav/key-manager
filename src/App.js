6c46a9896f2137f7e44c3cfb0ae349e3f1bf6059
abhijeetsourav <122fp0881@nitrkl.ac.in>
2023-11-02 15:27:59 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import "./components/signup/signup.css";
+import "./App.css";
+import React, {
+  useContext,
+  useRef,
+  useState,
+  useEffect,
+  useNavigate,
+} from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { auth, db } from "../src/firebase";
+
+import Login from "./components/login/login";
+import Signup from "./components/signup/signup";
+import Name from "./components/main/db_test";
+import LoadingSign from "./components/loader/loader";
+// import PrivateRoute from "./routes/privateroute";
+// import PublicRoute from "./routes/publicroute";
+
+import { AuthContext } from "./components/auth";
+import AdminPanel from "./components/admin/admin-panel";
+
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase";
+
+import push from "./components/notification";
 
 function App() {
+  const currentUser = useContext(AuthContext);
+  const docIn = useRef(null);
+  const [userIsAdmin, setUserIsAdmin] = useState(false);
+
+  // ***************Notification permission*********************//
+  async function notificationPermission() {
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+      //generate token
+      const token = await getToken(messaging, {
+        vapidKey:
+          "BFMPBroQEd4Bl5PV-VbCAaBlClizBohZrR-Nkr_G6odIU6jqkMhtyCLZssViUsk5TWtBtNWMoZ2sDAS73HNPy6w",
+      });
+      console.log("token == ", token);
+      push();
+    } else if (permission === "denied") {
+      alert("notification access was not given");
+    }
+  }
+
+  // useEffect(() => {
+  //   // requesting user to give permission
+  //   notificationPermission();
+  // }, []);
+
+  // Notification permission
+
+  // use effect block
+  useEffect(() => {
+    notificationPermission();
+    const func0 = async () => {
+      if (
+        ![
+          "App access pending...",
+          "App access declined...",
+          "Email verification pending in signup...",
+          "Email verification pending...",
+          "null",
+          "fetching...",
+        ].includes(currentUser)
+      ) {
+        const adminDocRef = doc(db, "admin-users", currentUser.id);
+        const adminDoc = await getDoc(adminDocRef);
+        //checking whether the user is admin or not
+        if (adminDoc.exists()) {
+          setUserIsAdmin(true);
+        } else {
+          setUserIsAdmin(false);
+        }
+      }
+
+      if (currentUser === "Email verification pending...") {
+        const func1 = async () => {
+          const adminUserDocRef = doc(db, "admin", auth.currentUser.uid);
+          const adminUserDoc = await getDoc(adminUserDocRef);
+          if (adminUserDoc.data()) {
+            docIn.current = "admin";
+          } else {
+            docIn.current = "nowhere";
+          }
+        };
+        func1();
+      }
+    };
+    func0();
+  }, []);
+
+  const getComponentToRenderLogin = () => {
+    if (
+      currentUser === "App access pending..." ||
+      currentUser === "App access declined..." ||
+      currentUser === "Email verification pending..." ||
+      currentUser === "null"
+    ) {
+      return <Login />;
+    } else if (currentUser === "Email verification pending in signup...") {
+      return <Navigate to="/signup" />;
+    } else {
+      return <Navigate to="/home" />;
+    }
+  };
+  const getComponentToRenderSignup = () => {
+    if (
+      currentUser === "null" ||
+      currentUser === "Email verification pending in signup..."
+    ) {
+      return <Signup />;
+    } else if (
+      currentUser === "App access pending..." ||
+      currentUser === "App access declined..." ||
+      currentUser === "Email verification pending..."
+    ) {
+      return <Navigate to="/" />;
+    } else {
+      return <Navigate to="/home" />;
+    }
+  };
+  const getComponentToRenderAdmin = () => {
+    if (
+      currentUser === "App access pending..." ||
+      currentUser === "App access declined..." ||
+      currentUser === "Email verification pending..." ||
+      currentUser === "null"
+    ) {
+      return <Navigate to="/" />;
+    } else if (currentUser === "Email verification pending in signup...") {
+      return <Navigate to="/signup" />;
+    } else {
+      if (userIsAdmin) {
+        return <AdminPanel />;
+      } else {
+        return <Navigate to="/home" />;
+      }
+    }
+  };
+  const getComponentToRenderHome = () => {
+    if (
+      currentUser === "App access pending..." ||
+      currentUser === "App access declined..." ||
+      currentUser === "Email verification pending..." ||
+      currentUser === "null"
+    ) {
+      return <Navigate to="/" />;
+    } else if (currentUser === "Email verification pending in signup...") {
+      return <Navigate to="/signup" />;
+    } else {
+      return <Name />;
+    }
+  };
+
+  //***************** use effect block ends ********************//
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {currentUser === "fetching..." ? (
+        <LoadingSign />
+      ) : (
+        <Routes>
+          <Route exact path="/" element={getComponentToRenderLogin()} />
+          <Route path="/signup" element={getComponentToRenderSignup()} />
+          <Route path="/admin" element={getComponentToRenderAdmin()} />
+          <Route path="/home" element={getComponentToRenderHome()} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      )}
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "./components/auth";
+import { Toaster } from "react-hot-toast";
+import NetworkError from "./network-error/networkerror";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function CheckConnection() {
+  const [isConnectedToInternet, setIsConnectedToInternet] = useState(
+    navigator.onLine
+  );
+
+  useEffect(() => {
+    setIsConnectedToInternet(navigator.onLine);
+  }, [navigator.onLine]);
+
+  return isConnectedToInternet;
+}
+try {
+  root.render(
+    <React.StrictMode>
+      {navigator.onLine ? (
+        <>
+          <Toaster position="top-right" reverseOrder={false} />
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </>
+      ) : (
+        <NetworkError />
+      )}
+    </React.StrictMode>
+  );
+} catch (error) {
+  alert(error);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
